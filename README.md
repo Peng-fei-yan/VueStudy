26dd4acf40708588b69ed9ab6a33a304d4deeb15
Penf-fei-yan <2284845813@qq.com>
2018-07-14 00:52:09 +0800

    一些配置文件更新

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## reStudy
-## This is daily vue contant
-## index:这里联系的是指令v-for的实例--对应的js文件是test.js
-### index1:你好
+# ReStudy
+# This is daily vue contant
+# index:这里联系的是指令v-for的实例--对应的js文件是test.js
+# index1:
--- js/test1.js
+++ js/test1.js
@@ -1,15 +1,19 @@
-
-var data = { a: 1 }
+var data = {
+    a: 1
+}
 var vm = new Vue({
     el: '#app',
-    data: data
+    data: data,
+    
 })
 
 //vm.$data === data // => true
 //vm.$el === document.getElementById('example') // => true
 
+
+
 // $watch 是一个实例方法
 vm.$watch('a', function (newValue, oldValue) {
     // 这个回调将在 `vm.a` 改变后调用
-    console.log('');
+    console.log('你好');
 })
