5940c3df2dbcc272db07fd26b4d2b2d9cfe56cdd
Penf-fei-yan <2284845813@qq.com>
2018-07-16 01:55:19 +0800

    vue一个小例子

--- vuejs-guide4.5/js/test2.js
+++ vuejs-guide4.5/js/test2.js
@@ -1,25 +1,33 @@
-
-var Child=Vue.extend({
-    props:['didi'],
-    template:'<div>{{didi}}</div>',
-    replace:true
-})
-
+//  var Child=Vue.extend({
+//     props:['didi'],
+//     template:'<div>{{didi}}</div>',
+//     replace:true
+// })
 
 
-var Parent=Vue.extend({
-    template:'<p>i am parent</p><br><child :didi="hello"></child>',
-    data:function(){
-       return{ 'hello':'hello'}
-    },
-    components:{
-        'child':Child
-    }
-})
+
+// var Parent=Vue.extend({
+//     template:'<p>i am parent</p><br><child :didi="hello"></child>',
+//     data:function(){
+//        return{ 'hello':'hello'}
+//     },
+//     components:{
+//         'child':Child
+//     }
+// }) 
 
 new Vue({
-    el:"#app",
-    components:{
-        'didi-p':Parent
+  el: '#app',
+  data: {
+    info: {
+      name: '顺风车'
     }
+  },
+  components: {
+    // 'didi-p':Parent
+    'child': {
+      props: ['msg'],
+      template: '<div>{{msg.name}}</div>'
+    }
+  }
 })
